54957883303129a17ad7a6fe927a5d4e101987cd
Sanket <sankethirani.sh@gmail.com>
2023-05-17 14:08:04 +1200

    Update reverseString with solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    //my solution
+    const textArray = text.split("");
+    let reverseText = "";
+    for (i = textArray.length - 1; i >= 0; i--){
+        reverseText += textArray[i];
+    }
+    return reverseText;
 
+    //better way of doing it
+    return text.split("").reverse().join("");
+    //Split the text into an array of characters
+    //Reverse the array using array.reverse() method
+    //Make a string of the character array using the join() method
 };
 
 // Do not edit below this line
