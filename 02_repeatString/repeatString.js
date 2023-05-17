22cb6c328dfb872f2e03f897202917052c12f9c7
Sanket <sankethirani.sh@gmail.com>
2023-05-17 13:51:34 +1200

    Update repeatString.js with solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, noOfRepeats) {
+    let returnString = '';
+    if (noOfRepeats < 0){
+        return 'ERROR';
+    }
+    else{
+        for (i = 1; i <= noOfRepeats; i++){
+            returnString += inputString;
+        }
+        return returnString;
+    }
+    
 };
 
 // Do not edit below this line
