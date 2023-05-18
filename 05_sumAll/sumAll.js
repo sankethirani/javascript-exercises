7d2821ca35df090b092627d4f856bbcc0e9735a5
Sanket <sankethirani.sh@gmail.com>
2023-05-18 14:37:47 +1200

    Update sumAll.js with solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let sum = 0;
 
+    if (value1 < 0 ||
+        value2 < 0 ||
+        !Number.isInteger(value1) ||
+        !Number.isInteger(value2)){
+            return "ERROR";
+    }
+    else{
+        let smallerValue;
+        let largerValue;
+
+        if (value1 > value2){
+            largerValue = value1;
+            smallerValue = value2;
+        }
+        else{
+            largerValue = value2;
+            smallerValue = value1;
+        }
+
+        for(i = smallerValue; i <= largerValue; i++){
+            sum += i;
+        }
+        
+        return sum;
+    }
+
+    
 };
 
 // Do not edit below this line
