486e1c5e70b898a66714f093ba73fda93fdf8e4b
Sanket <sankethirani.sh@gmail.com>
2023-05-18 14:50:36 +1200

    Update leapYears.js with solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 4 === 0) && !(year % 100 === 0) ){
+        return (true);
+    }
+    else if ((year % 100 === 0) && (year % 400 === 0)){
+        return (true);
+    }
+    else{
+        return (false);
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+
+// year % 4 = 0 is a leap year but year % 100 = 0 is not unless
+// year % 100 = 0 is not a leap year unless year % 400 = 0
