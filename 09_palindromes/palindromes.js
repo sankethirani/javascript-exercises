3c4cebe1f107492107b232c5bf50ce9a3e93b81a
Sanket <sankethirani.sh@gmail.com>
2023-07-27 13:58:31 +1200

    Update with solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const trimmedString = string.replaceAll(",", "")
+        .replaceAll(".","")
+        .replaceAll("?", "")
+        .replaceAll("'", "")
+        .replaceAll("!","")
+        .replaceAll(" ", "")
+        .toLowerCase();
+    
+    return trimmedString === trimmedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
