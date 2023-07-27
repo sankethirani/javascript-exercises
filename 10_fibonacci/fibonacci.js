5c4d05b3f120efc748dab7ff2c6c6d300b022628
Sanket <sankethirani.sh@gmail.com>
2023-07-27 14:44:20 +1200

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // let sequence = [1,1];
+    // if (!isNaN(n) && n > 0){
+    //     for (i = 1; i <= n; i++){
+    //         sequence.push(sequence[i] + sequence[i-1]);
+    //     }
+    //     return (sequence[sequence.length - 3]);
+    // }
+    // else{
+    //     return("OOPS");
+    // }
 
+    //better solution
+    if (isNaN(n)) return false;
+    if(n < 0) return "OOPS";
+    if(n === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (i = 1; i < n; i++){
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
