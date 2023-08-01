2143094337dcb64e7989ce227909a428fd3b5f81
Sanket <sankethirani.sh@gmail.com>
2023-08-01 12:51:21 +1200

    Add solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
-};
+    // My solution
 
+    // return books.reduce((titleList, currentBook) => {
+    //     titleList.push(currentBook.title);
+    //     return titleList;
+    // }, []);
+
+    // Much cleaner and nicer solution
+    return books.map(book => book.title);
+}
 // Do not edit below this line
 module.exports = getTheTitles;
